8a81399911a00bb734bb92edd2ce57f5627ee446
Jim Kriz <jim@kriz.net>
2021-10-21 09:33:56 -0400

    Implement ArrayFind
    - ArrayFind will locate the element's position in the array, not just telling oyu whether the item exists or not
    - Refactor ArrayContainsItem to use ArrayFind

--- Solutions/C#/BinarySearch/BinarySearch.Tests/SearchTest.cs
+++ Solutions/C#/BinarySearch/BinarySearch.Tests/SearchTest.cs
@@ -18,7 +18,7 @@ namespace BinarySearch.Tests
         [DataRow(null)]
         [DataRow(new int[0])]
         [TestMethod]
-        public void SearchNullOrEmptyArrayShouldReturnFalse(int[] array)
+        public void ArrayContainsItemNullOrEmptyArrayShouldReturnFalse(int[] array)
         {
             Assert.IsFalse(fixture.ArrayContainsItem(array, 1));
         }
@@ -27,7 +27,7 @@ namespace BinarySearch.Tests
         [DataRow(new int[1] { 1 }, 1, true)]
         [DataRow(new int[1] { 2 }, 1, false)]
         [TestMethod]
-        public void SearchSingleItemArrayShouldReturnTrueIfItemEqualsTheElement(int[] array, int item, bool expectedResult)
+        public void ArrayContainsItemSingleItemArrayShouldReturnTrueIfItemEqualsTheElement(int[] array, int item, bool expectedResult)
         {
             Assert.AreEqual(expectedResult, fixture.ArrayContainsItem(array, item), $"Array [ {(string.Join(",", array))} ] should {(expectedResult ? "" : "not")} contain {item}");
         }
@@ -37,7 +37,7 @@ namespace BinarySearch.Tests
         [DataRow(new int[] { 1, 3, 5 }, 5, true)]
         [DataRow(new int[] { 1, 3, 5 }, 6, false)]
         [TestMethod]
-        public void SearchItemIsFirstOrLastElementInArrayShouldReturnTrue(int[] array, int item, bool expectedResult)
+        public void ArrayContainsItemItemIsFirstOrLastElementInArrayShouldReturnTrue(int[] array, int item, bool expectedResult)
         {
             Assert.AreEqual(expectedResult, fixture.ArrayContainsItem(array, item), $"Array [ {(string.Join(",", array))} ] should {(expectedResult ? "" : "not")} contain {item}");
         }
@@ -49,7 +49,7 @@ namespace BinarySearch.Tests
         [DataRow(new int[] { 1, 3, 5 }, 5, true)]
         [DataRow(new int[] { 1, 3, 5 }, 6, false)]
         [TestMethod]
-        public void SearchItemIsAnywhereInArrayShouldReturnTrue(int[] array, int item, bool expectedResult)
+        public void ArrayContainsItemItemIsAnywhereInArrayShouldReturnTrue(int[] array, int item, bool expectedResult)
         {
             Assert.AreEqual(expectedResult, fixture.ArrayContainsItem(array, item), $"Array [ {(string.Join(",", array))} ] should {(expectedResult ? "" : "not")} contain {item}");
         }
@@ -59,7 +59,7 @@ namespace BinarySearch.Tests
         [DataRow(10000)]
         [DataRow(100000)]
         [TestMethod]
-        public void SearchLargeArrayOfRandomNumbersShouldReturnTrueIfItemIsFound(int numberOfElements)
+        public void ArrayContainsItemLargeArrayOfRandomNumbersShouldReturnTrueIfItemIsFound(int numberOfElements)
         {
             var random = new Random();
             var array = Enumerable.Repeat(0, numberOfElements).Select(i => random.Next()).ToArray();
@@ -67,6 +67,61 @@ namespace BinarySearch.Tests
             var chosenElement = array[random.Next(0, numberOfElements)];
 
             Assert.IsTrue(fixture.ArrayContainsItem(array, chosenElement), $"Large array should contain {chosenElement}");
+        }
+
+        [DataRow(null)]
+        [DataRow(new int[0])]
+        [TestMethod]
+        public void ArrayFindNullOrEmptyArrayShouldReturnFalse(int[] array)
+        {
+            Assert.AreEqual(-1, fixture.ArrayFind(array, 1));
+        }
+
+
+        [DataRow(new int[1] { 1 }, 1, 0)]
+        [DataRow(new int[1] { 2 }, 1, -1)]
+        [TestMethod]
+        public void ArrayFindSingleItemArrayShouldReturnTrueIfItemEqualsTheElement(int[] array, int item, int expectedPosition)
+        {
+            Assert.AreEqual(expectedPosition, fixture.ArrayFind(array, item), $"Array [ {(string.Join(",", array))} ] should {(expectedPosition >= 0 ? "" : "not")} contain {item} {(expectedPosition >= 0 ? "at position {expectedResult}" : "")}");
+        }
+
+        [DataRow(new int[] { 1, 3, 5 }, 0, -1)]
+        [DataRow(new int[] { 1, 3, 5 }, 1, 0)]
+        [DataRow(new int[] { 1, 3, 5 }, 5, 2)]
+        [DataRow(new int[] { 1, 3, 5 }, 6, -1)]
+        [TestMethod]
+        public void ArrayFindItemIsFirstOrLastElementInArrayShouldReturnTrue(int[] array, int item, int expectedPosition)
+        {
+            Assert.AreEqual(expectedPosition, fixture.ArrayFind(array, item), $"Array [ {(string.Join(",", array))} ] should {(expectedPosition >= 0 ? "" : "not")} contain {item} {(expectedPosition >= 0 ? "at position {expectedResult}" : "")}");
+        }
+
+        [DataRow(new int[] { 1, 3, 5 }, 0, -1)]
+        [DataRow(new int[] { 1, 3, 5 }, 2, -1)]
+        [DataRow(new int[] { 1, 3, 5 }, 1, 0)]
+        [DataRow(new int[] { 1, 3, 5 }, 3, 1)]
+        [DataRow(new int[] { 1, 3, 5 }, 5, 2)]
+        [DataRow(new int[] { 1, 3, 5 }, 6, -1)]
+        [TestMethod]
+        public void ArrayFindItemIsAnywhereInArrayShouldReturnTrue(int[] array, int item, int expectedPosition)
+        {
+            Assert.AreEqual(expectedPosition, fixture.ArrayFind(array, item), $"Array [ {(string.Join(",", array))} ] should {(expectedPosition >= 0 ? "" : "not")} contain {item} {(expectedPosition >= 0 ? "at position {expectedResult}" : "")}");
+        }
+
+
+        [DataRow(5001)]
+        [DataRow(10000)]
+        [DataRow(100000)]
+        [TestMethod]
+        public void ArrayFindLargeArrayOfRandomNumbersShouldReturnTrueIfItemIsFound(int numberOfElements)
+        {
+            var random = new Random();
+            var array = Enumerable.Repeat(0, numberOfElements).Select(i => random.Next()).ToArray();
+            Array.Sort(array);
+            var chosenElementIndex = random.Next(0, numberOfElements);
+            var chosenElement = array[chosenElementIndex];
+
+            Assert.AreEqual(chosenElementIndex, fixture.ArrayFind(array, chosenElement), $"Large array should contain {chosenElement} at position {chosenElementIndex}");
         }
     }
 }
--- Solutions/C#/BinarySearch/BinarySearch/Search.cs
+++ Solutions/C#/BinarySearch/BinarySearch/Search.cs
@@ -6,28 +6,41 @@ namespace BinarySearch
     {
         public Boolean ArrayContainsItem(int[] array, int item)
         {
+            return ArrayFind(array, item, 0) >= 0;
+        }
+
+        public int ArrayFind(int [] array, int item)
+        {
+            return ArrayFind(array, item, 0);
+        }
+
+        private int ArrayFind(int [] array, int item, int offset)
+        {
             // Empty or null array will never contain the item
-            if(array == null || array.Length == 0)
+            if (array == null || array.Length == 0)
             {
-                return false;
+                return -1;
             }
 
             // A single-element array is an easy case to test for
-            if(array.Length == 1 && array[0] == item)
+            if (array.Length == 1 && array[0] == item)
             {
-                return true;
+                return 0 + offset;
             }
 
             // If the first element or the last element in the array matches our item, we can short-circuit the rest of the logic below (the item is in the array)
-            if(array[0] == item || array[^1] == item)
+            if (array[0] == item) 
             {
-                return true;
+                return 0 + offset;
+            } else if (array[^1] == item)
+            {
+                return array.Length - 1 + offset;
             }
 
             // If the item is outside the range of the array, we can short-circuit the rest of the logic below (the item is not in the array)
-            if(array[0] > item || array[^1] < item)
+            if (array[0] > item || array[^1] < item)
             {
-                return false;
+                return -1;
             }
 
             // The item falls within the range of the array, but may or may not be an element in the array.
@@ -36,11 +49,24 @@ namespace BinarySearch
             var middleElementIndex = (int)(Math.Ceiling(array.Length / 2.0)) - 1;
             var middleElementValue = array[middleElementIndex];
 
-            // This uses the new array indexing/ranges operators introduced with C# 8.0:
-            // https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-8#indices-and-ranges
-            var slice = item >= middleElementValue ? array[middleElementIndex ..] : array[0 .. middleElementIndex];
 
-            return ArrayContainsItem(slice, item);
+            // The '..' syntax below is the new array indexing/range operator introduced with C# 8.0:
+            // https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-8#indices-and-ranges
+
+            // Since we are recursing, and passing slices of the original array, we'll keep track of the offset from the start of
+            // the original array. This allows us to keep the code simple, while still being able to return the position of the 
+            // found element in the original array.
+            int[] slice;
+            if(item >= middleElementValue)
+            {
+                slice = array[middleElementIndex..];
+                offset += middleElementIndex;
+            } else
+            {
+                slice = array[0..middleElementIndex];
+            }
+
+            return ArrayFind(slice, item, offset);
         }
     }
 }
